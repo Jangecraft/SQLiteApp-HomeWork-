3d259c59f4429191909292b49263bd1fcbeb6bcf
Jangecraft <78905953+Jangecraft@users.noreply.github.com>
2021-04-23 09:29:05 +0700

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteApp
+{
+    class Database
+    {
+        public async static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sqliteSample.db")) //สร้างไฟล์ชื่อ sqliteSample.db
+            {
+                db.Open(); //สั่งเปิด
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader(); //สั่งรัน
+            }
+        }
+        public static void AddData(string inputText)
+        {
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Name);";
+                insertCommand.Parameters.AddWithValue("@Name", inputText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from MyTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+
+                db.Close();
+
+            }
+            return entries;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLiteApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<String> list;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Database.InitializeDatabase();
+            Database.AddData("ภูมิพัฒน์ โพธิแพทย์");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            list = Database.GetData();
+
+            foreach (String data in list)
+            {
+                MessageBox.Show(data);
+            }
+
+        }
+    }
+}
